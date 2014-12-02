fbbc201421a36d806ec7801afb1272bbaac14a5e
Bruce <bn22@uw.edu>
2014-12-02 14:45:02 -0800

    Deleted Log In Stuff

--- js/app.js
+++ js/app.js
@@ -2,59 +2,65 @@
  * Created by marcocheng on 11/26/14.
  */
 $(document).ready(function() {
-    $('#logout').click(function() {
-       Parse.User.logOut();
-        $('#logout').hide();
-        $('#signUp').show();
-        $('#logIn').show();
-    });
-
-    $('#submit').click(function () {
-
-        var user = new Parse.User();
-        user.set("username", $('#username').val());
-        user.set("password", $('#password').val());
-        user.set('email', $('email').val());
-        user.signUp(null, {
-            success: function (user) {
-                console.log("User signed up!");
+    $('#submit').click(function() {
+        var carbonEmission = Parse.Object.extend("CO2Emissions");
+        var CO2 =new carbonEmission();
+        CO2.set("Username", $('#username').val()) ;
+        CO2.set("Password", $('#password').val());
+        CO2.set("Email", $('#email').val());
+        CO2.save(null, {
+            success: function() {
+                alert('it works!');
+            },
+            error: function(CO2, error) {
+                console.log(error.message);
             }
         });
     });
 
-    $('#loginsubmit').click(function () {
-        Parse.User.logIn($('#loginusername').val(), $('#loginpassword').val(), {
-            success: function (user) {
-                console.log("User signed up!");
-                $('#logout').show();
-                $('#signUp').hide();
-                $('#logIn').hide();
-            }
-        });
+    $('body').scrollspy({ target: '.navbar-custom' });
 
 
-        $('body').scrollspy({target: '.navbar-custom'});
-
-
-        $('[data-spy="scroll"]').each(function () {
-            var $spy = $(this).scrollspy('refresh')
-        });
+    $('[data-spy="scroll"]').each(function () {
+        var $spy = $(this).scrollspy('refresh')
+    });
 
 //    $(document).ready(function() {
 //        $('#fullpage').fullpage();
 //
 //    });
 
-        var map;
-        var geocoder;
-        var directionsDisplay;
-        var directionsService = new google.maps.DirectionsService();
+    var map;
+    var geocoder;
+    var directionsDisplay;
+    var directionsService = new google.maps.DirectionsService();
 //  var addr1 = 'University of Washington, Seattle, WA';
 //  var addr2 = "Seattle University, Seattle, WA";
-        var addr1;
-        var addr2;
-        var mode;
-        var mapElem = document.getElementById('map');
+    var addr1;
+    var addr2;
+    var mode;
+    var mapElem = document.getElementById('map');
+    var center = {
+        lat: 47.6,
+        lng: -122.3
+    };
+    map = new google.maps.Map(mapElem, {
+        center: center,
+        zoom: 12
+    });
+    //We need to set start and end destinations, as well as travel mode.
+    //This should be done in the initialize function
+    $("#calculate").click(function() {
+        initialize();
+        addr1 = $("#startaddress").val();
+        addr2 = $("#endaddress").val();
+        placeMarkers();
+        calcRoute();
+        calculateDistances();
+    });
+
+    function initialize() {
+        directionsDisplay = new google.maps.DirectionsRenderer();
         var center = {
             lat: 47.6,
             lng: -122.3
@@ -63,176 +69,148 @@ $(document).ready(function() {
             center: center,
             zoom: 12
         });
-        //We need to set start and end destinations, as well as travel mode.
-        //This should be done in the initialize function
-        $("#calculate").click(function () {
-            initialize();
-            addr1 = $("#startaddress").val();
-            addr2 = $("#endaddress").val();
-            placeMarkers();
-            calcRoute();
-            calculateDistances();
+        geocoder = new google.maps.Geocoder();
+        directionsDisplay.setMap(map);
+        mode = "DRIVING";
+        addr1 = "";
+        addr2 = "";
+    }
+
+    function placeMarkers() {
+        geocoder.geocode({address: addr1}, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                var coords = results[0].geometry.location;
+                var marker = new google.maps.Marker({
+                    position: coords,
+                    map: map
+                });
+            }
+
         });
+        geocoder.geocode({address: addr2}, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                var coords = results[0].geometry.location;
+                var marker = new google.maps.Marker({
+                    position: coords,
+                    map: map
+                });
+            }
 
-        function initialize() {
-            directionsDisplay = new google.maps.DirectionsRenderer();
-            var center = {
-                lat: 47.6,
-                lng: -122.3
-            };
-            map = new google.maps.Map(mapElem, {
-                center: center,
-                zoom: 12
-            });
-            geocoder = new google.maps.Geocoder();
-            directionsDisplay.setMap(map);
-            mode = "DRIVING";
-            addr1 = "";
-            addr2 = "";
-        }
+        });
+    }
 
-        function placeMarkers() {
-            geocoder.geocode({address: addr1}, function (results, status) {
-                if (status == google.maps.GeocoderStatus.OK) {
-                    var coords = results[0].geometry.location;
-                    var marker = new google.maps.Marker({
-                        position: coords,
-                        map: map
-                    });
-                }
+    function calcRoute() {
+        var request = {
+            origin:addr1,
+            destination: addr2,
+            travelMode: google.maps.TravelMode.DRIVING
+        };
+        directionsService.route(request, function(response, status) {
+            if(status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+            }
+        });
+    }
 
-            });
-            geocoder.geocode({address: addr2}, function (results, status) {
-                if (status == google.maps.GeocoderStatus.OK) {
-                    var coords = results[0].geometry.location;
-                    var marker = new google.maps.Marker({
-                        position: coords,
-                        map: map
-                    });
-                }
+    function calculateDistances() {
+        var service = new google.maps.DistanceMatrixService();
+        service.getDistanceMatrix ({
+            origins: [addr1],
+            destinations: [addr2],
+            travelMode: google.maps.TravelMode.DRIVING,
+            unitSystem: google.maps.UnitSystem.IMPERIAL,
+            avoidHighways: false,
+            avoidTolls: false
+        }, callback);
+    }
 
-            });
-        }
+    function callback(response, status) {
+        if (status != google.maps.DistanceMatrixStatus.OK) {
+            alert('Error was: ' + status);
+        } else {
+            var origins = response.originAddresses;
+            var destinations = response.destinationAddresses;
+            var outputDiv = document.getElementById('outputDiv');
+            outputDiv.innerHTML = '';
 
-        function calcRoute() {
-            var request = {
-                origin: addr1,
-                destination: addr2,
-                travelMode: google.maps.TravelMode.DRIVING
-            };
-            directionsService.route(request, function (response, status) {
-                if (status == google.maps.DirectionsStatus.OK) {
-                    directionsDisplay.setDirections(response);
-                }
-            });
-        }
-
-        function calculateDistances() {
-            var service = new google.maps.DistanceMatrixService();
-            service.getDistanceMatrix({
-                origins: [addr1],
-                destinations: [addr2],
-                travelMode: google.maps.TravelMode.DRIVING,
-                unitSystem: google.maps.UnitSystem.IMPERIAL,
-                avoidHighways: false,
-                avoidTolls: false
-            }, callback);
-        }
-
-        function callback(response, status) {
-            if (status != google.maps.DistanceMatrixStatus.OK) {
-                alert('Error was: ' + status);
-            } else {
-                var origins = response.originAddresses;
-                var destinations = response.destinationAddresses;
-                var outputDiv = document.getElementById('outputDiv');
-                outputDiv.innerHTML = '';
-
-                for (var i = 0; i < origins.length; i++) {
-                    var results = response.rows[i].elements;
-                    for (var j = 0; j < results.length; j++) {
-                        outputDiv.innerHTML += origins[i] + ' to ' + destinations[j]
+            for (var i = 0; i < origins.length; i++) {
+                var results = response.rows[i].elements;
+                for (var j = 0; j < results.length; j++) {
+                    outputDiv.innerHTML += origins[i] + ' to ' + destinations[j]
                         + ': ' + results[j].distance.text + ' in '
                         + results[j].duration.text + '<br>';
-                        calculateEmissions(results[j].distance.value);
-                    }
+                    calculateEmissions(results[j].distance.value);
                 }
             }
         }
+    }
 
-        function calculateEmissions(distance) {
-            var result = 0;
-            if (mode == "DRIVING") {
-                result = (0.96 * distance);
-            } else if (mode == "TRANSIT") {
-                result = (0.64 * distance);
-            }
-            var total = (result / 1609.344);
-            var value = total.toFixed(2);
-            var emissions = document.getElementById('emissions');
-            emissions.innerHTML = "Total emissions: " + value;
+    function calculateEmissions(distance) {
+        var result = 0;
+        if(mode == "DRIVING") {
+            result = (0.96 * distance);
+        } else if(mode == "TRANSIT") {
+            result = (0.64 * distance);
         }
-    });
+        var total = (result / 1609.344);
+        var value = total.toFixed(2);
+        var emissions = document.getElementById('emissions');
+        emissions.innerHTML = "Total emissions: " + value;
+    }
+});
 
 //
 //$(document).ready(function() {
-    $('#fullpage').fullpage({
-        //Navigation
-        menu: false,
-        anchors: ['1', '2', '3', '4'],
+$('#fullpage').fullpage({
+    //Navigation
+    menu: false,
+    anchors:['1', '2','3','4'],
 //        menu: '#myMenu',
-        navigation: false,
-        navigationPosition: 'right',
-        navigationTooltips: ['firstSlide', 'secondSlide'],
-        slidesNavigation: true,
-        slidesNavPosition: 'bottom',
+    navigation: false,
+    navigationPosition: 'right',
+    navigationTooltips: ['firstSlide', 'secondSlide'],
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom',
 
-        //Scrolling
-        css3: true,
-        scrollingSpeed: 700,
-        autoScrolling: true,
-        scrollBar: false,
-        easing: 'easeInQuart',
-        easingcss3: 'ease',
-        loopBottom: false,
-        loopTop: false,
-        loopHorizontal: true,
-        continuousVertical: false,
-        normalScrollElements: '#element1, .element2',
-        scrollOverflow: false,
-        touchSensitivity: 15,
-        normalScrollElementTouchThreshold: 5,
+    //Scrolling
+    css3: true,
+    scrollingSpeed: 700,
+    autoScrolling: true,
+    scrollBar: false,
+    easing: 'easeInQuart',
+    easingcss3: 'ease',
+    loopBottom: false,
+    loopTop: false,
+    loopHorizontal: true,
+    continuousVertical: false,
+    normalScrollElements: '#element1, .element2',
+    scrollOverflow: false,
+    touchSensitivity: 15,
+    normalScrollElementTouchThreshold: 5,
 
-        //Accessibility
-        keyboardScrolling: true,
-        animateAnchor: true,
+    //Accessibility
+    keyboardScrolling: true,
+    animateAnchor: true,
 
-        //Design
-        verticalCentered: true,
-        resize: true,
-        sectionsColor: ['#393939', '#393939', '#393939', '#393939'],
-        paddingTop: '3em',
-        paddingBottom: '10px',
-        fixedElements: '#header, .footer',
-        responsive: 0,
+    //Design
+    verticalCentered: true,
+    resize : true,
+    sectionsColor : ['#393939', '#393939','#393939','#393939'],
+    paddingTop: '3em',
+    paddingBottom: '10px',
+    fixedElements: '#header, .footer',
+    responsive: 0,
 
-        //Custom selectors
-        sectionSelector: '.section',
-        slideSelector: '.slide',
+    //Custom selectors
+    sectionSelector: '.section',
+    slideSelector: '.slide',
 
-        //events
-        onLeave: function (index, nextIndex, direction) {
-        },
-        afterLoad: function (anchorLink, index) {
-        },
-        afterRender: function () {
-        },
-        afterResize: function () {
-        },
-        afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
-        },
-        onSlideLeave: function (anchorLink, index, slideIndex, direction) {
-        }
-    });
+    //events
+    onLeave: function(index, nextIndex, direction){},
+    afterLoad: function(anchorLink, index){},
+    afterRender: function(){},
+    afterResize: function(){},
+    afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+    onSlideLeave: function(anchorLink, index, slideIndex, direction){}
 });
 //});
